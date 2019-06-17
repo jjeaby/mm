86a752350488ea029597ddd5ea44af1c7900bf6b
jjeaby <jjeaby@gmail.com>
2019-06-17 10:06:32 +0900

    iphone long press event add

--- App.js
+++ App.js
@@ -6,106 +6,29 @@ import { WebView } from 'react-native-webview';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 import MainScreen from './Components/MainScreen';
 
+import { event } from './injectJS/event.js';
+
 
 export default class App extends React.Component {
-    render() {
-        let jsCode = `
-          var node = document;
-          var longpress = false;
-          var presstimer = null;
-          var longtarget = null;
-          
-          var cancel = function(e) {
-              if (presstimer !== null) {
-                  clearTimeout(presstimer);
-                  presstimer = null;
-              }
-          
-          };
-          
-          var click = function(e) {
-              if (presstimer !== null) {
-                  clearTimeout(presstimer);
-                  presstimer = null;
-              }
-          
-          
-              if (longpress) {
-                  return false;
-              }
-          
-              // alert("press");              
-              
-          };
-          
-          
-          var start = function(e) {
-              if (e.type === "click" && e.button !== 0) {
-                  return;
-              }
-              // alert(e.target.textContent);          
 
-              longpress = false;
-          
-              if (presstimer === null) {
-                  presstimer = setTimeout(function() {
-                      // alert("long click");
-                      
- 
-  const url = 'https://api.github.com/orgs/nodejs'
- 
-    try {
+    constructor(props){
 
-
-
-
-fetch(url, {
-  method: 'post',
-  headers: {
-    'Accept': 'application/json, text/plain, */*',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    id : 1,
-    src : e.target.textContent,
-    translate_type: 'enko',
-    document_type : 'test',
-    test_req: 'False',
-  })
-})
-.then(response => response.json())
-.then(data => {
-  // Prints result from \`response.json()\` in getRequest
-  // alert(data.login) 
-  // e.target.textContent = e.target.textContent +  '\\n'  + data.message;
-  e.target.textContent =   data.message;
-})
-
-    } catch (e) {
-      alert(e);
+        // alert(event.toString())
+        super(props);
+        // console.log(event.toString());
+        this.state = {
+            url: 'https://naver.com',
+            jsCode: event.toString() + 'event();',
+        }
     }
 
-                      // alert(e.target.textContent);          
-                      longpress = true;
-                      return;
-                  }, 130);
-              }
-          
-              return false;
-          }; 
-          
-          node.addEventListener("mousedown", start);
-          node.addEventListener("click", click);
-          node.addEventListener("touchstart", start);
-          node.addEventListener("mouseout", cancel);
-          node.addEventListener("touchend", cancel);
-          node.addEventListener("touchleave", cancel);
-          node.addEventListener("touchcancel", cancel);
-          
-          node.oncontextmenu = function (e) {
-            e.preventDefault();
-          };
-          `
+
+    componentDidMount() {
+
+     }
+
+
+    render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.title}>Web App</Text>
@@ -116,10 +39,11 @@ fetch(url, {
                         // update component to be aware of loading status
                         const { nativeEvent } = syntheticEvent;
                         this.isLoading = nativeEvent.loading;
+                        // this.state.jsCode = event.toString() + 'event();'
                     }}
                     onMessage={(event)=> console.warn(event.nativeEvent.data)}
                     javaScriptEnabled={true}
-                    injectedJavaScript={jsCode}
+                    injectedJavaScript={this.state.jsCode}
                 />
             </View>
 
